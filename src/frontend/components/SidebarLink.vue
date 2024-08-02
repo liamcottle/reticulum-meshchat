75c1a203990a88adef322416384460920e9197d3
liamcottle <liam@liamcottle.com>
2024-08-02 20:17:41 +1200

    implement vue router

---
+++ src/frontend/components/SidebarLink.vue
@@ -0,0 +1,32 @@
+<template>
+    <RouterLink :to="to" v-slot="{ href, route, navigate, isActive, isExactActive }" custom>
+        <a :href="href" @click="handleNavigate($event, navigate)" type="button" :class="[ isExactActive ? 'bg-blue-100 text-blue-800 group:text-blue-800' : 'hover:bg-gray-100']" class="w-full text-gray-800 group flex gap-x-3 rounded-r-full p-2 mr-2 text-sm leading-6 font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
+            <span class="my-auto">
+                <slot name="icon"></slot>
+            </span>
+            <span class="my-auto">
+                <slot name="text"></slot>
+            </span>
+        </a>
+    </RouterLink>
+</template>
+
+<script>
+export default {
+    name: 'SidebarLink',
+    props: {
+        to: Object,
+    },
+    methods: {
+        handleNavigate(event, navigate) {
+
+            // emit click event for SidebarLink element
+            this.$emit('click');
+
+            // handle navigation
+            navigate(event);
+
+        },
+    },
+};
+</script>
--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import App from './components/App.vue';
+import AboutPage from "./components/about/AboutPage.vue";
+import SettingsPage from "./components/settings/SettingsPage.vue";
+import NetworkVisualiserPage from "./components/network/NetworkVisualiserPage.vue";
 
-createApp(App).mount('#app');
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        { path: '/' },
+        { path: '/about', name: "about", component: AboutPage },
+        { path: '/settings', name: "settings", component: SettingsPage },
+        { path: '/network-visualiser', name: "network-visualiser", component: NetworkVisualiserPage },
+    ],
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app');
