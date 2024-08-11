dabb41d6e5371e741251414ab51a8f223cf43801
liamcottle <liam@liamcottle.com>
2024-08-11 22:13:02 +1200

    add loading state ui

--- src/frontend/components/network-visualiser/NetworkVisualiser.vue
+++ src/frontend/components/network-visualiser/NetworkVisualiser.vue
@@ -1,6 +1,19 @@
 <template>
-    <div class="flex-1 h-full min-w-full sm:min-w-[500px]">
+    <div class="flex-1 h-full min-w-full sm:min-w-[500px] relative">
+
+        <!-- network -->
         <div id="network" class="w-full h-full"></div>
+
+        <!-- loading -->
+        <div v-if="isLoading" class="absolute flex top-0 bottom-0 left-0 right-0">
+            <div class="mx-auto my-auto">
+                <svg class="animate-spin h-6 w-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -20,13 +33,14 @@ export default {
     data() {
         return {
             config: null,
+            isLoading: false,
             interfaces: [],
             pathTable: [],
             announces: {},
         };
     },
     mounted() {
-        this.update();
+        this.init();
     },
     methods: {
         async getInterfaceStats() {
@@ -72,6 +86,10 @@ export default {
                 // do nothing if failed to load announces
                 console.log(e);
             }
+        },
+        async init() {
+            this.isLoading = true;
+            await this.update();
         },
         async update() {
 
@@ -217,7 +235,7 @@ export default {
 
             // create network ui
             const container = document.getElementById("network");
-            new Network(container, {
+            const network = new Network(container, {
                 nodes: nodes,
                 edges: edges,
             }, {
@@ -253,6 +271,11 @@ export default {
 
                     },
                 },
+            });
+
+            // hide loading once done
+            network.on("stabilized", () => {
+                this.isLoading = false;
             });
 
         },
