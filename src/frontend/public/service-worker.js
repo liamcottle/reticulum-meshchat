a94ce1ad508fcd8a7aedeeb06928db0789d25e09
liamcottle <liam@liamcottle.com>
2024-09-22 19:39:01 +1200

    add service worker and webmanifest to allow pwa install for android when using termux

---
+++ src/frontend/public/service-worker.js
@@ -0,0 +1,4 @@
+self.addEventListener('fetch',function() {
+    // this is required to meet the requirements for an installable pwa
+    // it allows the browser to ask the user if they want to install to their homescreen
+});
