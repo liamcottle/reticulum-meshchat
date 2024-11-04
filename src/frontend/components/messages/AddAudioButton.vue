74979f2a796880447b69535ff74976d8dda94aff
liamcottle <liam@liamcottle.com>
2024-11-04 18:45:28 +1300

    don't wrap button text to new line

--- src/frontend/components/messages/AddAudioButton.vue
+++ src/frontend/components/messages/AddAudioButton.vue
@@ -16,7 +16,7 @@
                 <path d="M7 4a3 3 0 0 1 6 0v6a3 3 0 1 1-6 0V4Z" />
                 <path d="M5.5 9.643a.75.75 0 0 0-1.5 0V10c0 3.06 2.29 5.585 5.25 5.954V17.5h-1.5a.75.75 0 0 0 0 1.5h4.5a.75.75 0 0 0 0-1.5h-1.5v-1.546A6.001 6.001 0 0 0 16 10v-.357a.75.75 0 0 0-1.5 0V10a4.5 4.5 0 0 1-9 0v-.357Z" />
             </svg>
-            <span class="ml-1 hidden sm:inline-block">Add Voice</span>
+            <span class="ml-1 hidden sm:inline-block whitespace-nowrap">Add Voice</span>
         </button>
 
         <div class="relative block">
