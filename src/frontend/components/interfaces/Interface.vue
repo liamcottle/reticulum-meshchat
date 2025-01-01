ddf144688ec047574e9c5c951661ca53f732f5d4
liamcottle <liam@liamcottle.com>
2025-01-02 00:21:28 +1300

    add enable and disable button to interface dropdown menu

--- src/frontend/components/interfaces/Interface.vue
+++ src/frontend/components/interfaces/Interface.vue
@@ -115,6 +115,26 @@
                     </template>
                     <template v-slot:items>
 
+                        <!-- enable/disable interface button -->
+                        <div class="border-b">
+
+                            <!-- enable interface button -->
+                            <DropDownMenuItem v-if="isInterfaceEnabled(iface)" @click="disableInterface">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
+                                    <path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v9a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM6.166 5.106a.75.75 0 0 1 0 1.06 8.25 8.25 0 1 0 11.668 0 .75.75 0 1 1 1.06-1.06c3.808 3.807 3.808 9.98 0 13.788-3.807 3.808-9.98 3.808-13.788 0-3.808-3.807-3.808-9.98 0-13.788a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
+                                </svg>
+                                <span>Disable Interface</span>
+                            </DropDownMenuItem>
+
+                            <DropDownMenuItem v-else @click="enableInterface">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
+                                    <path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v9a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM6.166 5.106a.75.75 0 0 1 0 1.06 8.25 8.25 0 1 0 11.668 0 .75.75 0 1 1 1.06-1.06c3.808 3.807 3.808 9.98 0 13.788-3.807 3.808-9.98 3.808-13.788 0-3.808-3.807-3.808-9.98 0-13.788a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
+                                </svg>
+                                <span>Enable Interface</span>
+                            </DropDownMenuItem>
+
+                        </div>
+
                         <!-- edit interface button -->
                         <DropDownMenuItem @click="editInterface">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
